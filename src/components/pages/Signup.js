9a54933347eea23c13bcc0fa3d04a1ff8bb1f9f6
OrkunAvci <orkunavci28@gmail.com>
2021-06-11 00:44:15 +0300

    Clean up

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React from 'react'
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export class Signup extends React.Component {
 	state = {
